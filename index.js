8c4dd1e5dee53e28b1de3262c0bbccd4c964614f
Brandon <bcharris713@gmail.com>
2022-04-30 13:12:09 -0500

    commit tree 115bf158dcfd47f3206fa842ecd5d811964ef93e

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = 'BOB';
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(name) {
+    bestCustomer = name;
+}
+
+const leastFavoriteCustomer = 'phil';
+
+function changeLeastFavoriteCustomer(name) {
+    leastFavoriteCustomer = name;
+}
